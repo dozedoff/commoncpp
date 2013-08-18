6befc6088bfe344a775fcf03039ed30617651802
dozedoff <dozedoffagain@gmail.com>
2013-08-19 00:59:32 +0200

    Changed ImagePHash test for higher resolution (from 49 bit to 64 bit)

--- src/test/hash/ImagePHashTest.cpp
+++ src/test/hash/ImagePHashTest.cpp
@@ -14,9 +14,9 @@
 using namespace Magick;
 
 TEST(ImagePHashTest, hashImage) {
-	ImagePHash iph;
+	ImagePHash iph(32,9);
 	long pHash = iph.getLongHash("src/test/hash/testImage.jpg");
-	ASSERT_EQ(7655956633439617023,pHash);
+	ASSERT_EQ(-2059292927385518081,pHash);
 }
 
 int main(int argc, char **argv) {
