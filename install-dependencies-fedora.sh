645b60910aa958441623c073ab782c3057109e4b
dozedoff <dozedoffagain@gmail.com>
2014-02-11 13:46:47 +0100

    Removed arch suffix from packages

--- install-dependencies-fedora.sh
+++ install-dependencies-fedora.sh
@@ -1,1 +1,1 @@
-sudo yum install gcc-c++.x86_64 boost-build.noarch boost-devel.x86_64 log4cplus-devel.x86_64 openssl-devel.x86_64 GraphicsMagick-c++-devel.x86_64 gtest-devel.x86_64
+sudo yum install gcc-c++ boost-build.noarch boost-devel log4cplus-devel openssl-devel GraphicsMagick-c++-devel gtest-devel
