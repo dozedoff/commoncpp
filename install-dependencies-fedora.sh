53eb0cc43e851e8b9783d7574022664020742f4a
Nicholas Wright <dozedoffagain@gmail.com>
2015-04-08 12:58:01 +0200

    Removed build tools from dependencies

--- install-dependencies-fedora.sh
+++ install-dependencies-fedora.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo yum install gcc-c++ cmake boost-devel log4cplus-devel openssl-devel GraphicsMagick-c++-devel gtest-devel
+sudo yum install boost-devel log4cplus-devel openssl-devel GraphicsMagick-c++-devel gtest-devel
