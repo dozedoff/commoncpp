5e02fbdeed0c41285b00d0144cd75ddb0a94c4b7
Nicholas Wright <dozedoffagain@gmail.com>
2015-04-08 13:12:13 +0200

    Copy compiled gTest library to /usr/local/lib instead of /usr/lib

--- install-dependencies-ubuntu.sh
+++ install-dependencies-ubuntu.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sudo apt-get install libboost-all-dev liblog4cplus-dev libgraphicsmagick++1-dev libssl-dev libgtest-dev
-cd /usr/src/gtest && sudo cmake CMakeLists.txt -Dgtest_disable_pthreads=ON && sudo make && sudo chmod o+x libgtest* && sudo mv libgtest* /usr/lib/
+cd /usr/src/gtest && sudo cmake CMakeLists.txt -Dgtest_disable_pthreads=ON && sudo make && sudo chmod o+x libgtest* && sudo mv libgtest* /usr/local/lib/
