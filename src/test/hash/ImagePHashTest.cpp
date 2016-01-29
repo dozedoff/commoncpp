10f37c47fdfa88fde26ace1baa1b90081349cf6a
Nicholas Wright <dozedoffagain@gmail.com>
2016-01-29 13:28:26 +0100

    Test for generating hash from image in buffer

--- src/test/hash/ImagePHashTest.cpp
+++ src/test/hash/ImagePHashTest.cpp
@@ -14,6 +14,7 @@
 #include <iostream>
 #include "hash/ImagePHash.hpp"
 #include <GraphicsMagick/Magick++.h>
+#include <fstream>
 
 TEST_CASE("hashImage", "[ImagePHashTest]") {
 	ImagePHash iph(32, 9);
@@ -30,6 +31,19 @@ TEST_CASE( "hashImage_resized", "[ImagePHashTest]") {
 TEST_CASE( "hashImage_default_settings", "[ImagePHashTest]") {
 	ImagePHash iph;
 	long pHash = iph.getLongHash("src/test/hash/testImage.jpg");
+	REQUIRE(pHash == -7276023159344816079);
+}
+
+TEST_CASE( "hashImage_binary_data", "[ImagePHashTest]") {
+	ImagePHash iph;
+	std::ifstream ifs("src/test/hash/testImage.jpg");
+	ifs >> std::noskipws;
+	std::istream_iterator<unsigned char> begin(ifs),end;
+	std::vector<unsigned char> image_data(begin,end);
+	std::copy(begin,end,std::back_inserter(image_data));
+
+
+	long pHash = iph.getLongHash(image_data);
 	REQUIRE(pHash == -7276023159344816079);
 }
 
